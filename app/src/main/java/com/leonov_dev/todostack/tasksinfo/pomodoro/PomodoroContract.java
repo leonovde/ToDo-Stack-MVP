8d24609fe79dd720d9be2c235ee0104f3e84c04f
leonovde <xormeradek@gmail.com>
2018-05-04 19:45:36 +0800

    Add progress bar drawable circle

--- app/src/main/java/com/leonov_dev/todostack/tasksinfo/pomodoro/PomodoroContract.java
+++ app/src/main/java/com/leonov_dev/todostack/tasksinfo/pomodoro/PomodoroContract.java
@@ -1,4 +1,21 @@
 package com.leonov_dev.todostack.tasksinfo.pomodoro;
 
-public class PomodoroContract {
+import com.leonov_dev.todostack.BasePresenter;
+import com.leonov_dev.todostack.BaseView;
+import com.leonov_dev.todostack.tasksinfo.TasksInfoContract;
+
+public interface PomodoroContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setTimer();
+    }
+
+
+    interface Presenter extends BasePresenter<View> {
+
+        void startTimer();
+
+    }
+
 }
